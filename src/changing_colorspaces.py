3746fb8cbb41c9326ce209abc41ede2e20cc3056
KevinChiu_flow <fsd83811@gmail.com>
2021-08-02 10:51:17 +0800

    imread

--- src/changing_colorspaces.py
+++ src/changing_colorspaces.py
@@ -1,14 +1,16 @@
 import cv2 as cv
 import numpy as np
-cap = cv.VideoCapture(0)
+# cap = cv.VideoCapture(0)
+cap = cv.imread("img\smarties.png")
 while(1):
     # Take each frame
-    _, frame = cap.read()
+    # _, frame = cap.read()
+    frame = cap
     # Convert BGR to HSV
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    lower_blue = np.array([92,134,69])
+    upper_blue = np.array([131,255,255])
     # Threshold the HSV image to get only blue colors
     mask = cv.inRange(hsv, lower_blue, upper_blue)
     # Bitwise-AND mask and original image
