a3c06b7a25138f775968ce6c4dd2e25e50eb7ef6
KevinChiu_flow <fsd83811@gmail.com>
2021-08-05 17:33:40 +0800

    find center again array error

--- src/track_img.py
+++ src/track_img.py
@@ -12,12 +12,12 @@ frame = cap.copy()
 # Convert BGR to HSV
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 # define range of blue color in HSV
-lower_blue = np.array([92,134,69])
-upper_blue = np.array([131,255,255])
+lower_blue = np.array([92, 134, 69])
+upper_blue = np.array([131, 255, 255])
 # Threshold the HSV image to get only blue colors
 mask = cv2.inRange(hsv, lower_blue, upper_blue)
 # Bitwise-AND mask and original image
-res = cv2.bitwise_and(frame,frame, mask= mask)
+res = cv2.bitwise_and(frame, frame, mask=mask)
 
 # cv2.imshow('frame',frame)
 # cv2.imshow('mask',mask)
@@ -27,10 +27,11 @@ res = cv2.bitwise_and(frame,frame, mask= mask)
 # findContours
 ###################################################################################
 imgResult = cap.copy()
-# gray = cv2.cv2tColor(res,cv2.COLOR_BGR2GRAY)  
-# ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)  
-contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)  
-cv2.drawContours(imgResult,contours,-1,(0,0,255),1)  
+# gray = cv2.cv2tColor(res,cv2.COLOR_BGR2GRAY)
+# ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
+contours, hierarchy = cv2.findContours(
+    mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(imgResult, contours, -1, (0, 0, 255), 1)
 
 # cv2.imshow('gray',gray)
 # cv2.imshow('imgResult',imgResult)
@@ -38,16 +39,41 @@ cv2.drawContours(imgResult,contours,-1,(0,0,255),1)
 ###################################################################################
 # find center
 ###################################################################################
+print(contours[0])
+points = []
 for c in contours:
-	# calculate moments for each contour
-	M = cv2.moments(c)
-	cX = int(M["m10"] / M["m00"])
-	cY = int(M["m01"] / M["m00"])
-	
-    # calculate x,y coordinate of center
-	cv2.circle(imgResult, (cX, cY), 2, (0, 0, 255), -1)
-	cv2.putText(imgResult, 'x:'+str(cX)+' y:'+str(cY), (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1)
-cv2.imshow('imgResult',imgResult)
+    # calculate moments for each contour
+    M = cv2.moments(c)
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
 
+    point = [cX, cY]
+    points.append(point)
+
+    # calculate x,y coordinate of center
+    cv2.circle(imgResult, (cX, cY), 2, (0, 0, 255), -1)
+    cv2.putText(imgResult, 'x:'+str(cX)+' y:'+str(cY), (cX - 25,
+                cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1)
+
+print(points)
+cv2.imshow('imgResult', imgResult)
+
+###################################################################################
+# find center again
+###################################################################################
+M = cv2.moments(points)
+cX = int(M["m10"] / M["m00"])
+cY = int(M["m01"] / M["m00"])
+
+# calculate x,y coordinate of center
+cv2.circle(imgResult, (cX, cY), 2, (0, 0, 255), -1)
+cv2.putText(imgResult, 'x:'+str(cX)+' y:'+str(cY), (cX - 25,
+            cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1)
+
+cv2.imshow('imgResult', imgResult)
+
+###################################################################################
+# end
+###################################################################################
 cv2.waitKey(0)
 cv2.destroyAllWindows()
