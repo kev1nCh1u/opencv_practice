7f83c6afda3797dfee57c34accf09f29b3d139a8
KevinChiu_flow <fsd83811@gmail.com>
2021-08-05 14:21:56 +0800

    find center

--- src/track_img.py
+++ src/track_img.py
@@ -1,7 +1,7 @@
-import cv2 as cv
+import cv2
 import numpy as np
-# cap = cv.VideoCapture(0)
-cap = cv.imread("img\smarties.png")
+# cap = cv2.VideoCapture(0)
+cap = cv2.imread("img\smarties.png")
 
 ###################################################################################
 # HSV mask
@@ -10,36 +10,44 @@ cap = cv.imread("img\smarties.png")
 # _, frame = cap.read()
 frame = cap.copy()
 # Convert BGR to HSV
-hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 # define range of blue color in HSV
 lower_blue = np.array([92,134,69])
 upper_blue = np.array([131,255,255])
 # Threshold the HSV image to get only blue colors
-mask = cv.inRange(hsv, lower_blue, upper_blue)
+mask = cv2.inRange(hsv, lower_blue, upper_blue)
 # Bitwise-AND mask and original image
-res = cv.bitwise_and(frame,frame, mask= mask)
+res = cv2.bitwise_and(frame,frame, mask= mask)
 
-cv.imshow('frame',frame)
-cv.imshow('mask',mask)
-cv.imshow('res',res)
-
-###################################################################################
-# findContours
-###################################################################################
-img = cap.copy()
-# gray = cv.cvtColor(res,cv.COLOR_BGR2GRAY)  
-# ret, binary = cv.threshold(gray,127,255,cv.THRESH_BINARY)  
-contours, hierarchy = cv.findContours(mask,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)  
-cv.drawContours(img,contours,-1,(0,0,255),1)  
-
-# cv.imshow('gray',gray)
-cv.imshow('img',img)
+# cv2.imshow('frame',frame)
+# cv2.imshow('mask',mask)
+# cv2.imshow('res',res)
 
 ###################################################################################
 # findContours
 ###################################################################################
+imgResult = cap.copy()
+# gray = cv2.cv2tColor(res,cv2.COLOR_BGR2GRAY)  
+# ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)  
+contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)  
+cv2.drawContours(imgResult,contours,-1,(0,0,255),1)  
 
+# cv2.imshow('gray',gray)
+# cv2.imshow('imgResult',imgResult)
 
+###################################################################################
+# find center
+###################################################################################
+for c in contours:
+	# calculate moments for each contour
+	M = cv2.moments(c)
+	cX = int(M["m10"] / M["m00"])
+	cY = int(M["m01"] / M["m00"])
+	
+    # calculate x,y coordinate of center
+	cv2.circle(imgResult, (cX, cY), 2, (0, 0, 255), -1)
+	cv2.putText(imgResult, 'x:'+str(cX)+' y:'+str(cY), (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1)
+cv2.imshow('imgResult',imgResult)
 
-cv.waitKey(0)
-cv.destroyAllWindows()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
