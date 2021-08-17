12a5142612db0b35bb58816fd6a50b3d5bc759e4
kevin <fsd83811@gmail.com>
2021-08-17 20:14:26 +0800

    ir turn bug

--- src/ir/ir_pose.py
+++ src/ir/ir_pose.py
@@ -6,7 +6,8 @@ import glob
 print(cv2.__version__)
 
 # cap = cv2.VideoCapture(0)
-cap = cv2.imread("img\ir_led_4.bmp")
+fname = "img\ir_led_4_turn.bmp"
+cap = cv2.imread(fname)
 
 ########################################################################################
 # load calibration file
@@ -32,8 +33,8 @@ img = cap.copy()
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # convert the grayscale image to binary image
 ret, thresh = cv2.threshold(gray_image, 80, 255, 0)
-cv2.imshow('gray_image', gray_image)
-cv2.imshow('thresh', thresh)
+# cv2.imshow('gray_image', gray_image)
+# cv2.imshow('thresh', thresh)
 # cv2.waitKey(0)
 mask = thresh.copy()
 
@@ -53,7 +54,7 @@ cv2.drawContours(imgResult, contours, -1, (0, 255, 0), 1)
 ###################################################################################
 # find circle center
 ###################################################################################
-# kevin value
+# kevin value points
 points = np.zeros((5, 1, 2), np.int32)
 i = 1
 
@@ -68,15 +69,7 @@ for c in contours:
     points[i][0] = point
     i += 1
 
-    # calculate x,y coordinate of center
-    cv2.circle(imgResult, (cX, cY), 2, (0, 255, 0), -1)
-    xy = 'x:' + str(cX) + ' y:' + str(cY)
-    cv2.putText(imgResult, xy, (cX + 4, cY + 2),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
-
 print('\n points: \n', points)
-
-cv2.imshow('imgResult', imgResult)
 
 ###################################################################################
 # find obj center
@@ -86,33 +79,34 @@ cX = int(M["m10"] / M["m00"])
 cY = int(M["m01"] / M["m00"])
 
 point = np.array([cX, cY], np.int32)
-print('point:',point)
+print('point:', point)
 points[0][0] = point
 print('\n points: \n', points)
-
-# calculate x,y coordinate of center
-cv2.circle(imgResult, (cX, cY), 2, (0, 0, 255), -1)
-xy = 'x:' + str(cX) + ' y:' + str(cY)
-cv2.putText(imgResult, xy, (cX + 4, cY + 2),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
-
-cv2.imshow('imgResult', imgResult)
 
 ###################################################################################
 # draw point
 ###################################################################################
+i = 0
+for p in points:
+	cX = p[0][0]
+	cY = p[0][1]
+    # calculate x,y coordinate of center
+	cv2.circle(imgResult, (cX, cY), 2, (0, 0, 255), -1)
+	xy = str(i) + ' x:' + str(cX) + ' y:' + str(cY)
+	cv2.putText(imgResult, xy, (cX + 15, cY + 2),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+	i += 1
+# cv2.imshow('imgResult', imgResult)
 
 ###################################################################################
 # find point end
 ###################################################################################
-cv2.waitKey(0)
+# cv2.waitKey(1000)
 cv2.destroyAllWindows()
 
 ########################################################################################
 # draw xyz
 ########################################################################################
-
-
 def draw(img, corners, imgpts):
     corner = tuple(corners[0].astype(np.int32).ravel())
     img = cv2.line(img, corner, tuple(
@@ -128,8 +122,12 @@ def draw(img, corners, imgpts):
 # pnp pose
 ########################################################################################
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# objp = np.zeros((4, 3), np.float32)
-# objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+
+corners2 = points.astype(np.float32)
+print('\n corners2:')
+print('corners2 type:', type(corners2))
+print('corners2 shape:', corners2.shape)
+print(corners2)
 
 objp = np.array([
 	(0.0, 0.0, 0.0),
@@ -138,39 +136,22 @@ objp = np.array([
     (1.0, 1.0, 0.0),
     (-1.0, -1.0, 0.0),
 ])
+print('\n objp: \n', objp)
 
 axis = np.float32([[2, 0, 0], [0, 2, 0], [0, 0, -2]]).reshape(-1, 3)
 
-for fname in glob.glob('img/calibration/WIN_20210810_09_40_14_Pro.jpg'):
-    # img = cv2.imread(fname)
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
+# Find the rotation and translation vectors.
+ret, rvecs, tvecs = cv2.solvePnP(objp, corners2, mtx, dist)
+# project 3D points to image plane
+imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
 
-    # if ret == True:
-    if True:
-            # corners2 = cv2.cornerSubPix(
-            #     gray, corners, (11, 11), (-1, -1), criteria)
 
-        corners2 = points.astype(np.float32)
-
-        print('\n corners2:')
-        print('corners2 type:', type(corners2))
-        print('corners2 shape:', corners2.shape)
-        print(corners2)
-
-        print('\n objp: \n', objp)
-
-        # Find the rotation and translation vectors.
-        # print('\n objp: \n', objp)
-        ret, rvecs, tvecs = cv2.solvePnP(objp, corners2, mtx, dist)
-        # project 3D points to image plane
-        imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
-        # imgpts, jac = cv2.projectPoints(
-        #     np.array([(0.0, 0.0, 10.0)]), rvecs, tvecs, mtx, dist)
-
-        imgResult = draw(imgResult, corners2, imgpts)
-        cv2.imshow('imgResult', imgResult)
-        k = cv2.waitKey(0) & 0xFF
-        if k == ord('s'):
-            cv2.imwrite(fname[:6]+ '_result' +'.png', imgResult)
+########################################################################################
+# end
+########################################################################################
+imgResult = draw(imgResult, corners2, imgpts)
+cv2.imshow('imgResult', imgResult)
+k = cv2.waitKey(0) & 0xFF
+if k == ord('s'):
+	cv2.imwrite(fname + '_result' +'.png', imgResult)
 cv2.destroyAllWindows()
