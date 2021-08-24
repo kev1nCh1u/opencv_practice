f6d1efa4fa05f6cb6c15ea0830aba443e8518dee
kevin <fsd83811@gmail.com>
2021-08-24 15:05:55 +0800

    stero

--- src/stereo_calibration/multi_capture.py
+++ src/stereo_calibration/multi_capture.py
@@ -2,7 +2,6 @@
 # multi_capture
 # by kevin
 ########################################################################################
-
 import cv2
 import numpy as np
 import yaml
@@ -12,21 +11,20 @@ import time
 print('\n opencv version:', cv2.__version__)
 
 ########################################################################################
-# load img
+# Capture img
 ########################################################################################
-if 1:
-    print('videoCapture....')
-    # cap = cv2.VideoCapture(0)
-    # cap2 = cv2.VideoCapture(1)
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    cap2 = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-    print('finish...\n')
-else:
-    fname = "img\ir_led_4_.bmp"
-    cap = cv2.imread(fname)
+print('videoCapture....')
+# cap = cv2.VideoCapture(0)
+# cap2 = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap2 = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+print('finish...\n')
 
+########################################################################################
+# read img
+########################################################################################
 while(True):
-
+    # catch time
     current_time = time.time()
 
     # 從攝影機擷取一張影像
@@ -39,6 +37,7 @@ while(True):
     
     inputKey = cv2.waitKey(1) & 0xFF
 
+    # if s save image
     if inputKey == ord('s'):
         cv2.imwrite('img/stereo_calibration/1/frame1_' + str(int(current_time)) + '.jpg', frame)
         cv2.imwrite('img/stereo_calibration/2/frame2_' + str(int(current_time)) + '.jpg', frame2)
@@ -50,6 +49,7 @@ while(True):
 
 # 釋放攝影機
 cap.release()
+ca2.release()
 
 # 關閉所有 OpenCV 視窗
 cv2.destroyAllWindows()
