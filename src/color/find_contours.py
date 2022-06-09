e57b8f09fd6a594bee52c23b6b0949d7d63b5815
KevinChiu <fs83811@gmail.com>
2022-06-09 23:09:23 +0800

    red-heart

--- src/color/find_contours.py
+++ src/color/find_contours.py
@@ -1,12 +1,17 @@
 import cv2  
  
 img = cv2.imread("img/contours.jpg")  
+img = cv2.imread("img/red-heart.png")  
+
  
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)  
-ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)  
+ret, binary = cv2.threshold(gray,50,255,cv2.THRESH_BINARY)  
+cv2.imshow("gray", gray)  
+cv2.imshow("binary", binary)  
  
 contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)  
-cv2.drawContours(img,contours,-1,(0,0,255),3)  
- 
+cv2.drawContours(img,contours,-1,(0,255,255),3)  
+print(contours)
+
 cv2.imshow("img", img)  
 cv2.waitKey(0)  
